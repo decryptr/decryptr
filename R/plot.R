29ee1bb1cf47886b342296ae92466f3c877d09bc
Julio Trecenti <julio.trecenti@gmail.com>
2017-05-13 03:51:11 -0300

    plot audio

--- R/plot.R
+++ R/plot.R
@@ -1,4 +1,3 @@
-
 #' Plot captcha
 #'
 #' @param x object
@@ -18,5 +17,24 @@ plot.image_captcha <- function(x, y, ...) {
     graphics::plot(grDevices::as.raster(png::readPNG(x)))
   }
 
+  stop("Wrong extention")
+}
+
+#' Plot captcha
+#'
+#' @param x object
+#' @param y -
+#' @param ... other
+#'
+#' @export
+plot.audio_captcha <- function(x, y, ...) {
+  # Get extention
+  ext <- tolower(tools::file_ext(basename(x)))
+  if (ext %in% c("jpeg", "jpg")) {
+    graphics::plot(grDevices::as.raster(jpeg::readJPEG(x)))
+  }
+  else if (ext == "png") {
+    graphics::plot(grDevices::as.raster(png::readPNG(x)))
+  }
   stop("Wrong extention")
 }
