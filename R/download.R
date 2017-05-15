a1b6ffd055d3a897c64727648ff32882bdd9e0a9
Julio Trecenti <julio.trecenti@gmail.com>
2017-05-15 08:34:03 -0300

    add destination to download fun

--- R/download.R
+++ R/download.R
@@ -5,21 +5,23 @@
 #' @param url url to download
 #' @param dest destination
 #' @param secure use ssl verifypeer as FALSE
+#' @param type image extension. Normally 'jpeg' or 'png'
 #'
 #' @export
-download <- function(url, dest = NULL, secure = FALSE) {
+download <- function(url, dest = NULL, secure = FALSE, type = 'jpeg') {
   # Build URL for request
   # If dest is null, create temporary file
+  httr::handle_reset(url)
+  .type <- paste0('.', type)
   if (is.null(dest)) {
-    dest <- tempfile(pattern = 'captcha', fileext = '.jpeg')
-  }   else {
-    dest <- tempfile(pattern = 'captcha', tmpdir = dest, fileext = '.jpeg')
+    dest <- tempfile(pattern = 'captcha', fileext = .type)
+  } else {
+    dest <- tempfile(pattern = 'captcha', tmpdir = dest, fileext = .type)
   }
   # Send get request
-  httr::GET(url, httr::write_disk(dest, overwrite = TRUE),
-            httr::config(ssl_verifypeer = secure))
+  httr::GET(url, httr::config(ssl_verifypeer = secure),
+            httr::write_disk(dest, overwrite = TRUE))
   read_captcha(dest)
-
 }
 
 download_tjrs <- function(dest = NULL) {
@@ -27,14 +29,14 @@ download_tjrs <- function(dest = NULL) {
   url <- paste0(
     'http://www.tjrs.jus.br/site_php/consulta',
     '/human_check/humancheck_showcode.php')
-  download(url)
+  download(url, dest)
 }
 
 
 download_tjmg <- function(dest = NULL) {
   # Build URL for request
   url <- 'http://www4.tjmg.jus.br/juridico/sf/captcha.svl'
-  download(url)
+  download(url, dest)
 }
 
 download_trt <- function(dest = NULL) {
@@ -42,19 +44,19 @@ download_trt <- function(dest = NULL) {
   url <- 'https://pje.trt3.jus.br/consultaprocessual/seam/resource/captcha'
   # Error in curl::curl_fetch_disk(url, x$path, handle = handle) :
   # Couldn't connect to server
-  download(url)
+  download(url, dest)
 }
 
 download_receita <- function(dest = NULL) {
   # Build URL for request
   url <- 'http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/cnpjreva_solicitacao2.asp'
-  download(url)
+  download(url, dest)
 }
 
 download_saj <- function(dest = NULL) {
   # Build URL for request
   url <- ('https://esaj.tjsp.jus.br/cjsg/imagemCaptcha.do')
-  download(url)
+  download(url, dest)
 }
 
 
@@ -62,7 +64,7 @@ download_tjrj <- function(dest = NULL) {
   # Build URL for request
   url <- ('http://www4.tjrj.jus.br/consultaProcessoWebV2/captcha')
   # If dest is null, create temporary file
-  download(url, secure = FALSE)
+  download(url, dest, secure = FALSE)
 }
 
 
