99fe6ec63df41b0e88f4fb0f99dbb923bbaeb3bd
jtrecenti <julio.trecenti@gmail.com>
2017-09-29 22:17:17 -0300

    better docs

--- R/download.R
+++ R/download.R
@@ -1,83 +1,99 @@
-#' Download captcha from any site
+#' Download captcha from a site
 #'
-#' Generic download function
+#' \code{captcha_download} is a generic function to download captchas from any
+#' site, given the url captcha. It is possible to download many captchas at
+#' onde with the \code{n} option. \code{captcha_download_*} are aliases for
+#' some known captchas of public services in Brazil.
 #'
-#' @param url url to download
-#' @param dest destination
-#' @param n number of captchas to download
-#' @param secure use ssl verifypeer as FALSE
-#' @param type image extension. Normally 'jpeg' or 'png'
+#' @param url url to download. Must be a valid url to download the captcha.
+#'   One may check whether the url is working or not running
+#'   \code{\link[httr]{BROWSE}(url)}.
+#' @param n total number of captchas to download. Defaults to one.
+#' @param dest captcha file destination. Defaults to current directory.
+#' @param secure use option \code{ssl_verifypeer = TRUE}? Defaults to \code{FALSE}.
+#' @param .default image extension if not extracted automatically from
+#'   \code{httr::GET(url, ...)$headers[["content-type"]]}.
+#'   Normally "jpeg" or "png". Defaults to "jpeg".
 #'
+#' All downloads have a timeout of three seconds to run. If the site you are
+#' accessing blocks IP after multiple calls, consider creating a loop an using
+#' \code{\link[base]{Sys.sleep}} to wait new calls.
+#'
+#' @examples
+#' \dontrun{
+#'   captcha_download_rfb()
+#'   captcha_download_trt(10, 'my/folder')
+#'   captcha_download("https://goo.gl/sqTQ4Z")
+#' }
 #' @export
-download <- function(url, dest, n, secure = FALSE, type = 'jpeg') {
+captcha_download <- function(url, n = 1, dest = ".", secure = FALSE,
+                             .default = "jpeg") {
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-  safe_download_one <- purrr::possibly(download_one, 'error')
+  safe_download_one <- purrr::safely(captcha_download_one)
   p <- progress::progress_bar$new(total = n)
   result <- purrr::map_chr(seq_len(n), ~{
-    result <- safe_download_one(url, dest, secure, type)
+    result <- safe_download_one(url, dest, secure, .default)
     p$tick()
-    result
+    result[!is.null(result)][[1]]
   })
   invisible(result)
 }
 
-download_one <- function(url, dest, secure, type) {
+captcha_download_one <- function(url, dest, secure, .default) {
   httr::handle_reset(url)
-  .type <- paste0('.', type)
-  dest <- tempfile(pattern = 'captcha', tmpdir = dest, fileext = .type)
+  dest_captcha <- tempfile(pattern = "captcha", tmpdir = dest)
   # Send get request
-  httr::GET(url, httr::config(ssl_verifypeer = secure),
-            httr::write_disk(dest, overwrite = TRUE))
-  dest
-}
-
-#' @rdname download
-#'
-#' @export
-download_tjrs <- function(dest = NULL, n = 1) {
-  url <- paste0(
-    'http://www.tjrs.jus.br/site_php/consulta',
-    '/human_check/humancheck_showcode.php'
+  r <- httr::GET(
+    url, httr::user_agent("R-decryptr"),
+    httr::config(ssl_verifypeer = secure),
+    httr::timeout(3)
   )
-  download(url, dest, n = n)
+  ct <- r$headers[["content-type"]]
+  if (is.null(ct)) ct <- .default
+  dest_captcha <- paste0(dest_captcha, ".", basename(ct))
+  writeBin(r$content, dest_captcha)
+  dest_captcha
 }
 
-#' @rdname download
+#' @rdname captcha_download
 #'
 #' @export
-download_tjmg <- function(dest = NULL, n = 1) {
-  url <- 'http://www4.tjmg.jus.br/juridico/sf/captcha.svl'
-  download(url, dest, n = n)
+captcha_download_tjrs <- function(n = 1, dest = ".") {
+  url <- paste0(
+    "http://www.tjrs.jus.br/site_php/consulta",
+    "/human_check/humancheck_showcode.php"
+  )
+  captcha_download(url, dest, n = n)
 }
 
-#' @rdname download
+#' @rdname captcha_download
 #'
 #' @export
-download_trt <- function(dest = NULL, n = 1) {
-  url <- 'https://pje.trt3.jus.br/consultaprocessual/seam/resource/captcha'
-  download(url, dest, n = n)
+captcha_download_tjmg <- function(n = 1, dest = ".") {
+  url <- "http://www4.tjmg.jus.br/juridico/sf/captcha.svl"
+  captcha_download(url, dest, n = n)
 }
 
-#' @rdname download
+#' @rdname captcha_download
 #'
 #' @export
-download_rfb <- function(dest = NULL, n = 1) {
-  url <- 'http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/captcha/gerarCaptcha.asp'
-  download(url, dest, n = n, type = "png")
+captcha_download_trt <- function(n = 1, dest = ".") {
+  url <- "https://pje.trt4.jus.br/consultaprocessual/seam/resource/captcha"
+  captcha_download(url, dest, n = n)
 }
 
-#' @rdname download
+#' @rdname captcha_download
 #'
 #' @export
-download_saj <- function(dest = NULL, n = 1) {
-  url <- ('https://esaj.tjsp.jus.br/cjsg/imagemCaptcha.do')
-  download(url, dest, n = n)
+captcha_download_rfb <- function(n = 1, dest = ".") {
+  url <- "http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/captcha/gerarCaptcha.asp"
+  captcha_download(url, dest, n = n)
 }
 
-#' @rdname download
+#' @rdname captcha_download
 #'
 #' @export
-download_tjrj <- function(dest = NULL, n = 1) {
-  url <- ('http://www4.tjrj.jus.br/consultaProcessoWebV2/captcha')
-  download(url, dest, n = n, secure = FALSE)
+captcha_download_tjrj <- function(n = 1, dest = ".") {
+  url <- ("http://www4.tjrj.jus.br/consultaProcessoWebV2/captcha")
+  captcha_download(url, dest, n = n)
 }
