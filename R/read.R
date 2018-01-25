e5f94aa9bf152300f57273e60711c3f0252d2c05
Caio Lente <ctlente@gmail.com>
2018-01-25 12:18:49 -0200

    read_captcha()'s path argument replaced with file

--- R/read.R
+++ R/read.R
@@ -6,21 +6,21 @@
 #' decryption. If `ans_in_path = TRUE`, will take the answer for the
 #' captchas from their filenames and get them ready for modeling.
 #'
-#' @param path Paths to one or more captcha images
+#' @param file Paths to one or more captcha images
 #' @param ans_in_path Whether or not the answers to the captchas are already
 #' in the paths to the files (separated by and underscore in the filename)
 #'
 #' @return A list of captcha objects
 #'
 #' @export
-read_captcha <- function(path, ans_in_path = FALSE) {
+read_captcha <- function(file, ans_in_path = FALSE) {
 
   # Check if files are images
-  ext <- tolower(tools::file_ext(basename(path)))
+  ext <- tolower(tools::file_ext(basename(file)))
   stopifnot(all(ext %in% c("jpeg", "jpg", "png")))
 
   # Iterate over files
-  out <- purrr::map(path, read_captcha_, ans_in_path)
+  out <- purrr::map(file, read_captcha_, ans_in_path)
   class(out) <- c("captcha")
 
   return(out)
@@ -28,38 +28,38 @@ read_captcha <- function(path, ans_in_path = FALSE) {
 
 #' Read a captcha file
 #'
-#' @param path Path to a captcha image
+#' @param file Path to a captcha image
 #' @param ans_in_path Whether or not the answer to the captcha is already
 #' in the path to the file (separated by and underscore in the filename)
 #'
-read_captcha_ <- function(path, ans_in_path) {
+read_captcha_ <- function(file, ans_in_path) {
 
   # Load captcha
-  captcha <- grey(load_image(path))
+  captcha <- grey(load_image(file))
 
   # Get answer from filename if necessary
-  answer <- if (ans_in_path) { get_answer(path) } else { NULL }
+  answer <- if (ans_in_path) { get_answer(file) } else { NULL }
 
   # Create captcha object
   captcha <- list(y = answer, x = captcha)
-  attr(captcha, "file") <- path
+  attr(captcha, "file") <- file
 
   return(captcha)
 }
 
 #' Read PNG or JPG
 #'
-#' @param path Path to image
+#' @param file Path to image
 #' @param ... Other arguments passed on to [png::readPNG()]
 #' or [jpeg::readJPEG()]
 #'
-load_image <- function(path, ...) {
+load_image <- function(file, ...) {
 
-  ext <- tolower(tools::file_ext(basename(path)))
+  ext <- tolower(tools::file_ext(basename(file)))
   if (ext %in% c("jpeg", "jpg")) {
-    img <- jpeg::readJPEG(path, ...)
+    img <- jpeg::readJPEG(file, ...)
   } else if (ext == "png") {
-    img <- png::readPNG(path, ...)
+    img <- png::readPNG(file, ...)
   }
 
   return(img)
@@ -75,13 +75,13 @@ grey <- function(img) {
 
 #' Get answer from filename and build answer matrix
 #'
-#' @param path Path to a captcha with its answer separated by and
+#' @param file Path to a captcha with its answer separated by and
 #' underscore ('_') in the filename
 #'
-get_answer <- function(path) {
+get_answer <- function(file) {
 
   # Collect answer from filename
-  answer <- basename(path) %>%
+  answer <- basename(file) %>%
     tools::file_path_sans_ext() %>%
     stringr::str_extract("_([a-zA-Z0-9]+)$") %>%
     stringr::str_sub(start = 2) %>%
