344db7484ac34cf7e1f501ee545be300490e8131
Caio Lente <ctlente@gmail.com>
2018-02-05 14:47:34 -0200

    Added tests for printing

---
+++ tests/testthat/test-print.R
@@ -0,0 +1,19 @@
+context("print")
+
+test_that("printing works", {
+  # Choose directory
+  path <- ifelse(dir.exists("test-captchas/"), "test-captchas/",
+                 "./tests/testthat/test-captchas/")
+
+  # Setup
+  files <- list.files(path, pattern = "_", full.names = TRUE)
+
+  # Read objects we want to print
+  cap <- read_captcha(files)
+  model <- load_model("trt")
+
+  # Expectations
+  expect_output(print(cap), "A list of 10.+01\\. \\\".*test-captchas//captcha")
+  expect_output(print(cap[[1]]), "A captcha.+\\\".*test-captchas//captcha")
+  expect_equal(class(plot(cap[[2]])), "raster")
+})
