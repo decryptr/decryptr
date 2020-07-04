e50d11e727de62744e2952f3330c29cdb1e607d9
C. Lente <7017340+clente@users.noreply.github.com>
2020-07-03 23:20:32 -0300

    Adapt tests to TF2.X

--- tests/testthat/test-decrypt.R
+++ tests/testthat/test-decrypt.R
@@ -2,35 +2,32 @@ context("decrypt")
 
 test_that("decryption works", {
 
-  if (!keras:::have_h5py())
-    skip("h5py not available for testing")
-
   # Choose directory
   path <- ifelse(dir.exists("sample-captchas/"), "sample-captchas/",
                  "./tests/testthat/sample-captchas/")
-  path2 <- ifelse(dir.exists("sample-captchas/"), "./sample-model.hdf5",
-                  "./tests/testthat/sample-model.hdf5")
+  path2 <- ifelse(dir.exists("sample-captchas/"), "./sample-model.tf",
+                  "./tests/testthat/sample-model.tf")
 
   # Setup
   files <- list.files(path, pattern = "_", full.names = TRUE)
   files <- files[stringr::str_length(files) > min(stringr::str_length(files))]
 
+  # Load model
+  model <- load_model(path2)
+
   # Read capthcas
   cap <- read_captcha(files)
 
   # Expectations
-  expect_equal(class(decrypt(files, load_model(path2))), "character")
+  expect_equal(class(decrypt(files, model)), "character")
   expect_equal(class(decrypt(files, path2)), "character")
   expect_equal(class(decrypt(files, "trt")), "character")
-  expect_equal(class(decrypt(cap, load_model(path2))), "character")
+  expect_equal(class(decrypt(cap, model)), "character")
   expect_equal(class(decrypt(cap, path2)), "character")
   expect_equal(class(decrypt(cap, "trt")), "character")
 })
 
 test_that("decrypt works with raw vector", {
-
-  if (!keras:::have_h5py())
-    skip("h5py not available for testing")
 
   path <- ifelse(dir.exists("sample-captchas/"), "sample-captchas/",
                  "./tests/testthat/sample-captchas/")
